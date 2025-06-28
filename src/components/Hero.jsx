66e2f1a5591344cd7d814057a9b08dc31aebc769
Surajit Tunga <surajittunga2005@gmail.com>
2025-06-28 11:12:42 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,10 @@ const Hero = () => {
     <div className='relative h-dvh w-screen overflow-x-hidden'>
         <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
             <div>
-                <div className='mask-clip-path'>
-
+                <div className='mask-clip-path absolute-center absolute z-50 size-64 bg-slate-600 cursor-pointer overflow-hidden rounded-lg'>
+                    <div>
+                        MiniVideoPlayer
+                    </div>
                 </div>
             </div>
         </div>
