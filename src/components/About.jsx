a7bca999e5287d503cbc9a6e22d98eefb12f2260
Surajit Tunga <surajittunga2005@gmail.com>
2025-06-30 18:26:32 +0530

    Complete About & Added Animated text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { ScrollTrigger } from "gsap/all";
+import AnimatedTitle from './AnimatedTitle';
 
 
 gsap.registerPlugin(ScrollTrigger);
@@ -32,9 +33,11 @@ useGSAP(()=> {
         <h1 className='font-general text-sm uppercase md:text-[10px]'>
             Welcome to Zentry
         </h1>
-        <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
-            Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure
-        </div>
+        <AnimatedTitle
+  title="Disc<b>o</b>ver the world's <br  /> l<b>a</b>rgest shared adventure"
+  containerClass="mt-5 text-5xl md:text-7xl lg:text-8xl !text-black text-center"
+/>
+
         <div className='about-subtext'>
             <p>The Games of Games begins-your life, now an epic MMORPG</p>
             <p>
